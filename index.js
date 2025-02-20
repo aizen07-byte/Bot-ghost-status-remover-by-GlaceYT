98b5c1d1222c7b278ec9b95e73cf98842b3e7b65
aizen07-byte <this.ezz.sushant@gmail.com>
2025-02-20 18:32:12 +0545

    sushant index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const client = new Client({
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  const imagePath = path.join(__dirname, 'index.html');
+  const imagePath = path.join(__dirname, 'index.html'); 
   res.sendFile(imagePath);
 });
 app.listen(port, () => {
